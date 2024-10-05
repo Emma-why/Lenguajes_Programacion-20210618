c7a439468883f19402cb095138f7a9bd6fbff861
Emma <126710550+Emma-why@users.noreply.github.com>
2024-10-05 14:26:12 +0000

    Update_2v_Palabra_Mod

--- palabra.py
+++ palabra.py
@@ -1,8 +1,8 @@
 def segunda_palabra_mas_larga(cadena):
     palabras = cadena.split()
-    palabras_ordenadas = sorted(palabras, key=lambda x: len(x), reverse=True)
+    palabras_ordenadas = sorted(palabras, key=lambda x: len(x))
     if len(palabras_ordenadas) >= 2:
-        return palabras_ordenadas[1] 
+        return palabras_ordenadas[1]
     return palabras_ordenadas[0]
 
-print(segunda_palabra_mas_larga("Oracion con varias palabras"))
+print(segunda_palabra_mas_larga("Palabras Oracion manzanasez cascodemaco"))
